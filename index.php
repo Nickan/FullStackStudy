611a3e6b38f40345e8c44b8c493610dcba09f332
Nickan <colete_nico@yahoo.com>
2017-03-24 21:11:10 +0800

    Initial setup.
    Tested and working.

---
+++ index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>jQuery Example</title>
+        <p onclick="$(this).hide();">This is a paragraph</p>
+        <script type="text/javascript" src="js/jquery-3.2.0.js">
+        </script>
+    </head>
+</html>
