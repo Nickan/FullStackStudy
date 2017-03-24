f304f9f321cf2eb724be0a207d5ed801627bfdf1
Nickan <colete_nico@yahoo.com>
2017-03-24 21:40:22 +0800

    Implemented Fade in feature
    Inlining style attribute is not recommended, use CSS instead.

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
         <title>jQuery Example</title>
     </head>
     <body>
-        <p id="paragraph">This is a paragraph</p>
+        <div id="message" style="display: none">Welcome to my website</div>
         <script type="text/javascript" src="js/jquery-3.2.0.js"></script>
-        <script type="text/javascript" src="js/hide.js"></script>
+        <script type="text/javascript" src="js/init.js"></script>
     </body>
 </html>
