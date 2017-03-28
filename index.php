c8af3ea8e04864f148595fd0896e6669750a2ea2
Nickan <colete_nico@yahoo.com>
2017-03-28 11:26:21 +0800

    HTML and CSS Review
    Creating facebook clone.

--- index.php
+++ index.php
@@ -2,11 +2,15 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>jQuery Example</title>
+        <title>Example Title</title>
     </head>
     <body>
-        <div id="message" style="display: none">Welcome to my website</div>
+        <div id="div">
+            <p>Paragraph</p>
+            <input type="button" value="Button"/>
+            <span>Span Text</span>
+        </div>
         <script type="text/javascript" src="js/jquery-3.2.0.js"></script>
-        <script type="text/javascript" src="js/init.js"></script>
+        <script type="text/javascript" src="js/file.js"></script>
     </body>
-</html>
+<html>
